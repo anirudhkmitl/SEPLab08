101092158565a9c5283b179341f9f0c567a94e64
Un <62011286@kmitl.ac.th>
2021-03-11 18:31:40 +0700

    [UPDATE] Finished Drawing program

--- program8_2.py
+++ program8_2.py
@@ -1,19 +1,84 @@
 import sys
-from PySide6.QtCore import *
-from PySide6.QtWidgets import *
-from PySide6.QtGui import *
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtCore import Qt
+
+class Canvas(QtWidgets.QLabel):
+
+    def __init__(self):
+        super().__init__()
+        pixmap = QtGui.QPixmap(600, 300)
+        self.setPixmap(pixmap)
+
+        self.last_x, self.last_y = None, None
+        self.pen_color = QtGui.QColor('#FFFFFF')
+
+    def set_pen_color(self, c):
+        self.pen_color = QtGui.QColor(c)
+
+    def mouseMoveEvent(self, e):
+        if self.last_x is None: # First event.
+            self.last_x = e.x()
+            self.last_y = e.y()
+            return # Ignore the first time.
+
+        painter = QtGui.QPainter(self.pixmap())
+        p = painter.pen()
+        p.setWidth(4)
+        p.setColor(self.pen_color)
+        painter.setPen(p)
+        painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
+        painter.end()
+        self.update()
+
+        # Update the origin for next time.
+        self.last_x = e.x()
+        self.last_y = e.y()
+
+    def mouseReleaseEvent(self, e):
+        self.last_x = None
+        self.last_y = None
+
+    def clear(self):
+        pixmap = QtGui.QPixmap(600, 300)
+        self.setPixmap(pixmap)
+
+        
+
+class QPaletteButton(QtWidgets.QPushButton):
+
+    def __init__(self, color):
+        super().__init__()
+        self.setFixedSize(QtCore.QSize(24,24))
+        self.color = color
+        self.setStyleSheet("background-color: %s;" % color)
 
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
         super().__init__()
 
-        self.label = QtWidgets.QLabel()
-        canvas = QtGui.QPixmap(400, 300)
-        self.label.setPixmap(canvas)
-        self.setCentralWidget(self.label)
+        self.canvas = Canvas()
 
-        self.last_x, self.last_y = None, None
+        w = QtWidgets.QWidget()
+        l = QtWidgets.QVBoxLayout()
+        w.setLayout(l)
+        l.addWidget(self.canvas)
+
+        palette = QtWidgets.QHBoxLayout()
+        self.add_palette_buttons(palette)
+        l.addLayout(palette)
+
+        self.setCentralWidget(w)
+
+    def add_palette_buttons(self, layout):
+        b = QtWidgets.QPushButton("Clear",self)
+        b.clicked.connect(self.clear)
+        layout.addWidget(b)
+        
+    def clear(self):
+        self.canvas.clear()
+        
+        
 
 
 app = QtWidgets.QApplication(sys.argv)
