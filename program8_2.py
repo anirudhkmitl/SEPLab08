2660c070a223c4cfd5b265eabfdd34407ba2547e
anirudhkmitl <79251361+anirudhkmitl@users.noreply.github.com>
2021-03-11 17:57:32 +0700

    [UPDATE] set to version py6

--- program8_2.py
+++ program8_2.py
@@ -3,20 +3,20 @@ from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
 
-        self.label = QtWidgets.QLabel()
-        canvas = QtGui.QPixmap(400, 300)
+        self.label = QLabel()
+        canvas = QPixmap(400, 300)
         self.label.setPixmap(canvas)
         self.setCentralWidget(self.label)
 
         self.last_x, self.last_y = None, None
 
 
-app = QtWidgets.QApplication(sys.argv)
+app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
 app.exec_()
